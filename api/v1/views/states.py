d5ad594d1f86cde0b1c0bc5ac14858a2990b53da
chaimaahassoune <chaimaa.22hassoune@gmail.com>
2023-10-30 22:05:09 +0000

    task6

---
+++ api/v1/views/states.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+""" objects that handle all default RestFul API actions for States """
+from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from models import storage, State
+
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def get_all_states():
+    states = [state.to_dict() for state in storage.all(State).values()]
+    return jsonify(states)
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_state(state_id):
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    return jsonify(state.to_dict())
+
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+def delete_state(state_id):
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    storage.delete(state)
+    storage.save()
+    return jsonify({}), 200
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def create_state():
+    data = request.get_json()
+    if not data:
+        abort(400, 'Not a JSON')
+    if 'name' not in data:
+        abort(400, 'Missing name')
+    new_state = State(**data)
+    new_state.save()
+    return jsonify(new_state.to_dict()), 201
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def update_state(state_id):
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    data = request.get_json()
+    if not data:
+        abort(400, 'Not a JSON')
+    for key, value in data.items():
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(state, key, value)
+    state.save()
+    return jsonify(state.to_dict()), 200
+
